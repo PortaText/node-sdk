00433c533299658129d2eaaebc2f781e4ee001ac
Marcelo Gornstein <marcelog@gmail.com>
2015-12-30 18:11:35 -0300

    adding test

---
+++ test/command_test.js
@@ -0,0 +1,12 @@
+var commandMod = require('../src/command/command');
+var assert = require('assert');
+
+describe('Command', function() {
+  describe('endpoint', function () {
+    it('should return error on abstract endpoint call', function (done) {
+      var command = new commandMod.Command();
+      assert.throws(command.endpoint, Error, 'not_implemented');
+      done();
+    });
+  });
+});
